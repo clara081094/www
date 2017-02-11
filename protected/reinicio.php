1433cdd5ebe7606c759e815866c7d05f4ae557e8
root <root@noreply.raspbx.org>
2017-02-10 22:10:55 -0500

    Ultimos html

--- protected/reinicio.php
+++ protected/reinicio.php
@@ -4,8 +4,10 @@ echo shell_exec("cp /etc/apache2/special/.htpasswd /etc/apache2/special/.ht");
 echo shell_exec("sleep 1");
 //copy('/etc/apache2/special/.htpasswd', '/etc/apache2/special/.ht');
 echo shell_exec("htpasswd -b /etc/apache2/special/.htpasswd administrador rawwwwwwww");
+echo shell_exec("service apache2 restart");
 echo shell_exec("sleep 9");
 echo shell_exec("cp /etc/apache2/special/.ht /etc/apache2/special/.htpasswd"); 
+
 header('Location: '."../seguridad");
 
 ?> 
